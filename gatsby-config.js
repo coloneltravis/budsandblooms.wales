214275c0f68f29dde976f9b7b29fcf8525b478da
Jake Bourne <jake.bourne@gmail.com>
2020-08-20 21:03:24 +0100

    Add page for Consent form link

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `Buds and Blooms Intergenerational Groups`,
@@ -35,8 +39,13 @@ module.exports = {
         display: `minimal-ui`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_API_KEY,
+        downloadLocal: true,
+      },
+    },
   ],
 }
---
+++ src/pages/consentform.js
@@ -0,0 +1,30 @@
+import React from "react"
+import Layout from "../components/layout"
+
+
+export default () => (
+
+    <Layout pageInfo={{ pageName: "Consent Form" }}>
+
+
+    <div>
+        <a href="javascript:void(
+        window.open(
+          'https://form.jotformeu.com/92166889344370',
+          'blank',
+          'scrollbars=yes,
+          toolbar=no,
+          width=1024,
+          height=800'
+        )
+      )
+    "> Buds and Blooms Consent Form </a>
+    </div>
+
+
+    <div>
+      <a href="https://form.jotformeu.com/92166889344370" rel="no-follow"><img src="https://www.jotform.com/uploads/accordionmagic/form_files/92166889344370_1597874496_qrcode_muse.png" width="20%" alt="QR Code for JotForm form"/></a>
+    </div>
+
+    </Layout>
+)
