d40492320a64eb031db093db7b013d48392c1bb6
Jake Bourne <jake.bourne@gmail.com>
2020-09-24 00:24:45 +0100

    Amend Resource Pack layout to include activities with images.

--- src/templates/resource-pack.js
+++ src/templates/resource-pack.js
@@ -1,28 +1,66 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { Img } from "gatsby-image"
 import Layout from "../components/layout"
+import { Container, Row, Col } from "react-bootstrap"
 
 export default ({ data }) => {
   const pack = data.contentfulResourcePacks
+
   return (
     <Layout pageInfo={{ pageName: pack.resourceTitle }}>
-      <div>
-        <div class="para-text" dangerouslySetInnerHTML={{ __html: pack.instructions.childMarkdownRemark.html }} />
-      </div>
+
+    <Container>
+      <Row>
+        <Col>
+          <div class="para-text" dangerouslySetInnerHTML={{ __html: pack.instructions.childMarkdownRemark.html }} />
+        </Col>
+      </Row>
+    </Container>
+
+    <Container>
+      {pack.activities != null && pack.activities.map(( activity ) => (      
+        <div>
+        <Row>
+          <Col><h3>{activity.activityTitle} </h3> </Col>
+        </Row>
+        <Row>
+          <Col md={8}>
+            <div class="para-text" dangerouslySetInnerHTML={{ __html: activity.activityDescription.childMarkdownRemark.html }} />
+          </Col>
+        <Col md={4}>
+              <img src={activity.activityImage.fluid.src} />
+         </Col>
+        </Row>
+        </div>
+      ))} 
+      </Container>
     </Layout>
   )
 }
 
 export const query = graphql`
   query($slug: String!) {
-    contentfulResourcePacks(slug: {eq: $slug}) {
-      id
+    contentfulResourcePacks(slug: {eq: $slug})
+    {
+      resourceTitle
       instructions {
         childMarkdownRemark {
           html
         }
       }
-      resourceTitle
+      activities {
+        activityTitle
+        activityDescription {
+          childMarkdownRemark {
+            html
+          }
+        }
+        activityImage {
+          fluid (maxWidth: 200) {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
     }
-  }
-`
+  }`
